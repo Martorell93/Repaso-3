c0469e31bb2a73275e85c378a7bb0b63a1f27fd9
Martorell93 <jaime.martorell.lopez@gmail.com>
2021-07-21 13:07:12 +0200

    feat (objeto punto 1): Creación objeto de la clase Punto

---
+++ main.ts
@@ -0,0 +1,7 @@
+//Import clase Punto
+import { Punto } from "./punto";
+
+//Creación objeto tipo Punto
+let punto1: Punto = new Punto(4,8);
+console.log("Imprimo por consola las coordenadas del punto:");
+console.log(punto1.toString());
